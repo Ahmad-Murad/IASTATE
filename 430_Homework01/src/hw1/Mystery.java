185945b2d4447bc13b1e423998e7ac1b2b528dc3
aguibert <andy.guibert@gmail.com>
2015-01-27 13:17:16 -0600

    430 - hw1 question 3

--- 430_Homework01/src/hw1/Mystery.java
+++ 430_Homework01/src/hw1/Mystery.java
@@ -1,13 +1,23 @@
 package hw1;
 
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * To make this class thread safe, first we chagne the next id variable to an
+ * AtomicLong which provides thread safe increments and gets. In addition, the
+ * 'id' variable is made final in the constructor. <br>
+ * To make the 'x' and 'prev' variables thread safe, we need to synchronize reads
+ * and writes amongst each other, to do this we synchronize the read 'get()' and
+ * 'update()' methods to make them acquire a lock on their own instance.
+ */
 public class Mystery
 {
   // every instance must have a distinct id 
-  private static long next = 0;  
+  private static AtomicLong next = new AtomicLong(0);  
   
   private int x;
   private int prev;
-  private long id;
+  private final long id;
   
   public Mystery(int initial)
   {
@@ -16,13 +26,13 @@ public class Mystery
     x = initial;
   }
   
-  public void update(int k)
+  public synchronized void update(int k)
   {
-    prev = x;
-    x = 31 * x + k;
+	  prev = x;
+	  x = 31 * x + k;
   }
   
-  public long get()
+  public synchronized long get()
   { 
     // prev in the left 32 bits, x in the right
     return (((long) prev) << 32) | x;
@@ -35,6 +45,6 @@ public class Mystery
   
   private long generateNextId()
   {
-    return next++;
+    return next.getAndIncrement();
   }
 }

76169629b3f959bcb3f94da8500959e58c301d51
aguibert <andy.guibert@gmail.com>
2015-01-27 12:20:41 -0600

    419 - build stuff

--- 419_Lab01/src/cpre419/lab01/Checksum.java
+++ 419_Lab01/src/cpre419/lab01/Checksum.java
@@ -1,23 +1,18 @@
 package cpre419.lab01;
 
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-import java.net.*;
-import org.apache.hadoop.fs.*;
-import org.apache.hadoop.conf.*;
-import org.apache.hadoop.io.*;
-import org.apache.hadoop.util.*;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 
-
-public class Checksum 
+public class Checksum
 {
-	public static void main(String args[]) throws Exception {
-		// The system configuration
-		Configuration conf = new Configuration();
-		
-		FileSystem fs = FileSystem.get(conf);
-		Path path = new Path("/class/s15419x/lab1/bigdata");
-		FSDataInputStream inp = fs.open(path);
-	}
+    public static void main(String args[]) throws Exception {
+        // The system configuration
+        Configuration conf = new Configuration();
+
+        FileSystem fs = FileSystem.get(conf);
+        Path path = new Path("/class/s15419x/lab1/bigdata");
+        FSDataInputStream inp = fs.open(path);
+    }
 }

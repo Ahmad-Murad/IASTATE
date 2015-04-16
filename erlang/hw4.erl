e3ae98eda15e39db7f3b34d1f63ea170b518be03
aguibert <andy.guibert@gmail.com>
2015-04-15 21:22:49 -0500

    430 - hw4 question 5 complete

--- erlang/hw4.erl
+++ erlang/hw4.erl
@@ -4,9 +4,9 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 -module(hw4).
--export([tailFib/1, listCompare/1, erlQuickSort/1, doRun/5, pi/3, calcPi/2]).
+-export([tailFib/1, listCompare/1, erlQuickSort/1, doRun/5, pi/3, calcPi/2, noDups/2]).
 
-%% question 1
+%% Question 1
 %% N : the Nth fibbonaci number to compute
 tailFib(0) ->
 	0;
@@ -16,7 +16,14 @@ tailFib(N) ->
 	tailFib(N-1) + tailFib(N-2).
 
 	
-%% question 2
+%% Question 2
+%% Adding elements to the head of the list is much more efficient than adding them to the tail.
+%% Comparison results:
+%% Array Size | Add to Head | Add to Tail
+%%  1000      | 1           | 1
+%%  10000     | 1           | 342998
+%%  100000    | 47000       | 39670839
+%% (times in microseconds)
 %% N : the size of the array to be randomly generated and sorted
 listCompare(N) ->
 	Start = erlang:now(),
@@ -43,7 +50,7 @@ addToTail(List, N) ->
 	addToTail(List ++ [random:uniform(100)], N-1).
 	
 	
-%% question 3
+%% Question 3
 %% Quick sort results:
 %% Array Size | Erlang | Java
 %%  100       | 1      | 206 
@@ -69,23 +76,30 @@ sort([Pivot|T]) ->
 sort([]) -> [].
 	
 	
-%% question 4
+%% Question 4
+%% This is the driver method that the user will type in.
+%% The number of samples and number of worker threads must be specified.
 calcPi(NumSamples, TotalThreads) ->
 	doRun(NumSamples, TotalThreads, TotalThreads, 0, 0).
 	
+%% All worker threads have been started and completed.  Compute Pi.
 doRun(NumSamples, TotalThreads, 0, 0, Total) ->
 	io:format("PI : ~w\n", [4.0 * Total / NumSamples]);
+%% All worker threads have been started.  Listen for messages.
 doRun(NumSamples, TotalThreads, 0, RunningThreads, Total) ->
 	receive
 		N ->
 			doRun(NumSamples, TotalThreads, 0, RunningThreads - 1, Total + N)
 	end;
+%% Start worker threads.
 doRun(NumSamples, TotalThreads, ToStart, RunningThreads, Total) ->
 	spawn(?MODULE, pi, [(NumSamples div TotalThreads), 0, self()]),
 	doRun(NumSamples, TotalThreads, ToStart-1, RunningThreads+1, 0).
 
+%% All samples complete.  Send result message to parent process for computation.
 pi(0, Count, ParentPid) ->
 	ParentPid ! Count;
+%% Compute 1 sample of pi.
 pi(NumSamples, Count, ParentPid) ->
 	{A1,A2,A3} = now(),
     random:seed(A1, A2, A3),
@@ -97,4 +111,18 @@ pi(NumSamples, Count, ParentPid) ->
 		true ->
 			pi(NumSamples - 1, Count, ParentPid)
 	end.
+	
+	
+%% Question 5
+noDups(Element, Acc) ->
+	if
+		Acc == [] ->
+			lists:append([Element], Acc);
+		hd(Acc) /= Element ->
+			lists:append([Element], Acc);
+		true ->
+			Acc
+	end.
+
+
 	
